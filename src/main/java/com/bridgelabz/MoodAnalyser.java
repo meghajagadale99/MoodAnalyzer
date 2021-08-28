7bad01c5c2ac37bc2400a8c50d813730a5e0a642
Megha <meghajagadale99@gmail.com>
2021-08-28 20:26:57 +0530

    Given a Message

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+
+    private String message;
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (message.contains("Sad")){
+            return "SAD";
+        }else{
+            return "HAPPY";
+        }
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+import org.junit.Assert;
+import org.junit.Test;
+public class MoodAnalyserTest {
+
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String mood =moodAnalyser.analyseMood();
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood =moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+}
