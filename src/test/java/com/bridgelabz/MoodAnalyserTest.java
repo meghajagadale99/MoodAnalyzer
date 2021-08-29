4f7b74f8b49492bfff149a3eec6213c662c23712
Megha <meghajagadale99@gmail.com>
2021-08-29 21:31:51 +0530

    Added: inform user if entered invalid mood

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,26 +1,31 @@
 package com.bridgelabz;
+
+import com.bridgelabz.MoodAnalysisException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
 public class MoodAnalyserTest {
 
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
-        String mood =moodAnalyser.analyseMood();
-        Assert.assertEquals("SAD",mood);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("SAD", mood);
     }
 
     @Test
     public void givenMessage_WhenHappy_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-        String mood =moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
     }
 
     @Test
     public void givenNullMood_ShouldThrowException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood =moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
     }
 }
