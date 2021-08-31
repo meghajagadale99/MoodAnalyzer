bae21eca2b50d961a4dc5b2ec3dc5f20e81520bc
Megha <meghajagadale99@gmail.com>
2021-08-31 18:21:41 +0530

    Added: Given a Message

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,25 +5,16 @@ import org.junit.Test;
 
 
 public class MoodAnalyserTest {
-
     @Test
-    public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+    public void givenMessage_WhenSad_ShouldReturn_Sad() {
         MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a Sad Message");
         String mood = moodAnalyzer.analyseMood();
         Assert.assertEquals("SAD",mood);
 
     }
-
     @Test
     public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is Happy Messgae");
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
-    }
-
-    @Test
-    public void givenNullMood_ShouldReturn_Happy() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser(null);
+        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is Happy Message");
         String mood = moodAnalyzer.analyseMood();
         Assert.assertEquals("HAPPY",mood);
     }
